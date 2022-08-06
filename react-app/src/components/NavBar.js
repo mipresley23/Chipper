814029c50d544685c52424c429e66c6c2a039998
Michael Presley <mipresley23@gmail.com>
2022-08-06 11:22:07 -0400

    bird logo added. home button done

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import { demoLogin } from '../store/session';
+import birdLogo from './assets/birdLogo.png'
+import birdHouse from './assets/chipper-home.png'
 
 const NavBar = () => {
   const dispatch = useDispatch();
@@ -16,9 +18,15 @@ const NavBar = () => {
   return (
     <nav>
       <ul id='side-bar-user-info-container'>
-        <li>
+        <li id='nav-home-logo-container'>
           <NavLink to='/' exact={true} activeClassName='active'>
-            Home
+            <img id='nav-home-logo' src={birdLogo} alt="Home"/>
+          </NavLink>
+        </li>
+        <li id='nav-home-birdhouse-container'>
+          <NavLink to='/' exact={true} activeClassName='active'>
+            <img id='nav-home-birdhouse' src={birdHouse} alt="Home"/>
+            <span id='nav-home-birdhouse-words'>Home</span>
           </NavLink>
         </li>
         {!sessionUser && (
